e7365af2228d363778daf7f4c7a3ede9592e9b03
wahyuseptiyan <wahyuseptiyan55@gmail.com>
2025-11-14 00:40:41 +0700

    Create CreditBy.java

---
+++ praktikum/week3-inheritance/src/main/java/com/upb/agripos/util/CreditBy.java
@@ -0,0 +1,9 @@
+package com.upb.agripos.util;
+
+public class CreditBy {
+    public static void print(String nim, String nama) {
+        System.out.println("\n=== Credit By ===");
+        System.out.println("NIM  : " + nim);
+        System.out.println("Nama : " + nama);
+    }
+}
