f5705a35ad8575915a08b56878c7a24b2ef7ee81
wahyuseptiyan <wahyuseptiyan55@gmail.com>
2025-11-13 22:38:30 +0700

    Update CreditBy.java

--- praktikum/week5-abstraction-interface/src/main/java/com/upb/agripos/util/CreditBy.java
+++ praktikum/week5-abstraction-interface/src/main/java/com/upb/agripos/util/CreditBy.java
@@ -1,1 +1,8 @@
+package main.java.com.upb.agripos.util;
 
+public class CreditBy {
+    public static void print(String nim, String nama) {
+        System.out.println("\n---");
+        System.out.println("Credit by: " + nim + " - " + nama);
+    }
+}
