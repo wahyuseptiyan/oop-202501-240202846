ff6b7160947d8ba10ded436f4dcb5832a2dd763b
wahyuseptiyan <wahyuseptiyan55@gmail.com>
2025-11-14 01:17:05 +0700

    Create CreditBy.java

---
+++ praktikum/week4-polymorphism/src/main/java/com/upb/agripos/util/CreditBy.java
@@ -0,0 +1,11 @@
+package com.upb.agripos.util;
+
+public class CreditBy {
+   
+     public static void print(String nim, String nama) {
+        System.out.println("\n=== Credit By ===");
+        System.out.println("Nama: " + nama);
+        System.out.println("NIM: " + nim);
+        System.out.println("=================\n");
+    }
+}
