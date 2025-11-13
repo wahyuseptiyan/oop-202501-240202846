d228fb7249baa3a54d165508a749ffa8068a6d6b
wahyuseptiyan <wahyuseptiyan55@gmail.com>
2025-11-14 01:13:07 +0700

    Create Benih.java

---
+++ praktikum/week4-polymorphism/src/main/java/com/upb/agripos/model/Benih.java
@@ -0,0 +1,15 @@
+package com.upb.agripos.model;
+
+public class Benih extends Produk {
+    private final String varietas;
+
+    public Benih(String kode, String nama, double harga, int stok, String varietas) {
+        super(kode, nama, harga, stok);
+        this.varietas = varietas;
+    }
+
+    @Override
+    public String getInfo() {
+        return "Benih  : " + super.getInfo() + ", Varietas: " + varietas;
+    }
+}
